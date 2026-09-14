ee25a248a439abad288588f5c03181b146e1d936
Preston Corless <pgattic@gmail.com>
2026-09-14 13:57:31 -0600

    Clamp colors

--- src/Color.hs
+++ src/Color.hs
@@ -5,8 +5,13 @@ import Vec3 (Vec3(..), (+^), (-^))
 
 type Color = Vec3
 
+clamp :: Double -> Double -> Double -> Double
+clamp low high x = max low (min high x)
+
 to256 :: Color -> (Word8, Word8, Word8)
-to256 (Vec3 r g b) = (floor (255.999 * r), floor (255.999 * g), floor (255.999 * b))
+to256 (Vec3 r g b) = let
+    conv x = floor (255.999 * (clamp 0 1 x))
+  in (conv r, conv g, conv b)
 
 printColor :: Color -> String
 printColor c = let (r, g, b) = to256 c in unwords [show (r), show (g), show (b)]
